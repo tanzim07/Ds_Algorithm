35eaf8a266f7e958f84c34d0155b6c83b4b28f0a
Tanzim Ahmed <tanzimahmed1994@gmail.com>
2022-11-25 21:56:51 +0600

    Agnostic binary search

--- searching/binarysearch.js
+++ searching/binarysearch.js
@@ -15,15 +15,31 @@
 
 // const testArr = [  2, 3, 5, 7, 8, 10, 13, 15, 18, 29, 30, 34, 38, 39, 41, 43, 45, 49, 56, 58, 58];
 const testArr = [2, 3, 5, 6, 7, 8, 10];
+
 // 8
 
-// const search = 58;
+// ascending order of array
+function binarySearch(nums, target) {
+  let start = 0;
+  let end = nums.length - 1;
+  while (start <= end) {
+    let mid = Math.floor((start + end) / 2);
+    if (nums[mid] === target) {
+      return mid;
+    } else if (target > nums[mid]) {
+      start = mid + 1;
+    } else {
+      end = mid - 1;
+    }
+  }
+  return -1;
+}
 
-const x = new Array()
-x.push(1)
-x.length
+console.log(binarySearch(testArr, 5));
 
-function binarySearch(arr, search) {
+// unknown order of array
+const dscArr = [12, 11, 9, 7, 3, 1];
+function binarySearch2(arr, search) {
   let start = 0;
   let end = arr.length - 1;
 
@@ -32,14 +48,24 @@ function binarySearch(arr, search) {
     if (arr[mid] === search) {
       console.log("found at index", mid);
       return mid;
-    } else if (search > arr[mid]) {
-      start = mid + 1;
     } else {
-      end = mid - 1;
+      if (arr[start] <= arr[end]) {
+        // 2,4,  6,   8,12,15,19,24,30
+        if (search < arr[mid]) {
+          end = arr[mid] - 1;
+        } else {
+          start = mid + 1;
+        }
+      } else {
+        if (search < arr[mid]) {
+          start = mid + 1;
+        } else {
+          end = mid - 1;
+        }
+      }
     }
   }
-  console.log("not found");
   return -1;
 }
-
-console.log(binarySearch(testArr, 21));
+// return binarySearch(arr, search);
+console.log(binarySearch2(dscArr, 7));
