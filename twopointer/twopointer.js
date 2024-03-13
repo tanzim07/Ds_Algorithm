e593e61732d19e3ef423ebe63f26c0af0eaf055a
tanzim077 <tanzimahmed1994@gmail.com>
2024-03-14 00:49:53 +0600

    Refactor targetIndices function in twopointer.js

--- twopointer/twopointer.js
+++ twopointer/twopointer.js
@@ -75,4 +75,80 @@ const x = function plusone(n) {
   return n + 1;
 };
 const y = [1, 2, 3];
-console.log(palindrom.jsMap(y, x));
+// console.log(palindrom.jsMap(y, x));
+
+class targetIndex {
+  // static targetIndices = function (nums, target) {
+  //   // 1. sort the array
+  //   // 2. Check the target is matched if matched return index in an array
+  //   const sortNums = nums.sort();
+  //   const targetIndex = [];
+  //   let start = 0;
+  //   let end = sortNums.length - 1;
+  //   while (start <= end) {
+  //     let mid = Math.floor((start + end) / 2);
+  //     if (sortNums[mid] === target) {
+  //       while (sortNums[mid] === target) {
+  //         targetIndex.push(mid);
+  //         console.log("🚀 ~ targetIndex ~ sortNums[mid]:", sortNums[mid]);
+  //         console.log("🚀 ~ targetIndex ~ sortNums[mid]:", sortNums[mid+1]);
+  //         // if (sortNums[mid] === target) {
+  //         //   targetIndex.push(mid);
+  //         // } else {
+  //         mid--;
+  //         // }
+  //       }
+  //       return targetIndex.sort();
+  //     } else if (target > sortNums[mid]) {
+  //       start = mid + 1;
+  //     } else {
+  //       end = mid - 1;
+  //     }
+  //   }
+  //   return [];
+  // };
+  /**
+   * @param {number[]} nums
+   * @param {number} target
+   * @return {number[]}
+   */
+  static targetIndices = function (nums, target) {
+    let resultArr = [];
+    if (!nums.includes(target)) return resultArr;
+
+    let sortedArray = nums;
+    sortedArray.sort((a, b) => a - b);
+    let left = 0;
+    let right = sortedArray.length - 1;
+    let start = -1;
+    let end = -1;
+
+    while (left <= right) {
+      let mid = Math.round((right + left) / 2);
+      if (sortedArray[mid] === target) {
+        start = mid - 1;
+        end = mid + 1;
+        resultArr.push(mid);
+        while (sortedArray[start] === target) {
+          resultArr.push(start);
+          start -= 1;
+        }
+        while (sortedArray[end] === target) {
+          resultArr.push(end);
+          end += 1;
+        }
+        break;
+      } else if (sortedArray[mid] < target) {
+        left = mid + 1;
+      } else if (sortedArray[mid] > target) {
+        right = mid - 1;
+      }
+    }
+    return resultArr.sort((a, b) => a - b);
+  };
+}
+// const nums = [1, 2, 5, 2, 3];
+const nums = [100, 1, 100];
+const target = 100;
+
+console.log(targetIndex.targetIndices(nums, target));
